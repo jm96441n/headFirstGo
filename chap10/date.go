0f9caa24918f3e9ec6b0f4811827c47705a1b48e
jm96441n <john@jmaguire.tech>
2020-03-24 15:17:07 -0400

    finished chap10

---
+++ chap10/date.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+        "errors"
+        "fmt"
+        "log"
+)
+
+type Date struct {
+        Year int
+        Month int
+        Day int
+}
+
+func (d *Date) SetYear(year int) error {
+        if year < 1 {
+                return errors.New("invalid year")
+        }
+        d.Year = year
+        return nil
+}
+
+func (d *Date) SetMonth(month int) error {
+        if month < 1 || month > 12{
+                return errors.New("invalid month")
+        }
+        d.Month = month
+        return nil
+}
+
+func (d *Date) SetDay(day int) error {
+        if day < 1 || day > 31 {
+                return errors.New("invalid day")
+        }
+        d.Day = day
+        return nil
+}
+
+func main() {
+        date := Date{}
+        err := date.SetYear(2019)
+        if err != nil {
+                log.Fatal(err)
+        }
+        err = date.SetMonth(5)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        err = date.SetDay(31)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        fmt.Println(date)
+}
