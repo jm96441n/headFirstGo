16a0e579c19b1b642df7de928c8051bd0a59cdb1
jm96441n <jmaguire@mdsol.com>
2020-03-26 08:45:06 -0400

    finished chap14

---
+++ chap14/prose/join_test.go
@@ -0,0 +1,32 @@
+package prose
+
+import (
+        "fmt"
+        "testing"
+)
+
+type testData struct {
+        list []string
+        want string
+}
+
+func TestJoinWithCommas(t *testing.T) {
+        tests := []testData{
+                testData{list: []string{}, want: ""},
+                testData{list: []string{"apple"}, want: "apple"},
+                testData{list: []string{"apple","orange"}, want: "apple and orange"},
+                testData{list: []string{"apple","orange","pear"}, want: "apple, orange, and pear"},
+        }
+
+        for _, test := range tests {
+                got := JoinWithCommas(test.list)
+                if got != test.want {
+                        t.Errorf(errorString(test.list, got, test.want))
+                }
+        }
+}
+
+func errorString (list []string, got string, want string) string {
+        return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+}
+
